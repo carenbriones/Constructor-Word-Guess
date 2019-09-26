6a51081991d8d78bab158978aa0b1e47e9e921de
Caren B <carenpbriones@gmail.com>
2019-09-26 12:53:08 -0700

    Allows user to play another game once their current game finishes

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ var inquirer = require("inquirer");
 var wordBank = ["happy", "energetic", "angry", "sleepy", "hungry", "upset"];
 var randomIndex = Math.floor(Math.random() * wordBank.length);
 var randomWord = new Word(wordBank[randomIndex]);
-const maxGuesses = 10;
-var guessesLeft = maxGuesses;
+const MAX_GUESSES = 10;
+var guessesLeft = MAX_GUESSES;
 
-console.log(randomWord.convertToString());
+
 playRound();
 
 // Asks user to guess the word
 function playRound() {
+    // Displays current state of guessed word
+    console.log("\n" + randomWord.convertToString() + "\n");
+
     inquirer.prompt([{
         name: "guess",
         message: "Guess a letter: ",
@@ -26,21 +29,40 @@ function playRound() {
         if (preGuess === postGuess) {
             console.log("\nIncorrect :(");
             guessesLeft -= 1;
+            console.log("Guesses left: " + guessesLeft + "\n");
         } else { // Else, word changed, so guess was correct
             console.log("\nCorrect!!!");
         }
 
-        console.log("\n" + postGuess + "\n");
-        console.log("Guesses left: " + guessesLeft + "\n");
-
         if (guessesLeft === 0) {
             // Game is over
             console.log("Game over :(");
+            promptNewGame();
             // Continues game if there are still blanks to fill in
         } else if (randomWord.convertToString().includes("_")) {
             playRound();
         } else { // No more blanks, player has won the game
-            console.log("You've won!");
+            console.log("You've won! \n");
+            promptNewGame();
         }
     })
+}
+
+function promptNewGame() {
+    inquirer.prompt([{
+        name: "playAgain",
+        message: "Would you like to play again?",
+        type: "confirm"
+    }]).then(function(inquirerResponse) {
+        if (inquirerResponse.playAgain) {
+            resetGame();
+            playRound();
+        }
+    })
+}
+
+function resetGame() {
+    guessesLeft = MAX_GUESSES;
+    randomIndex = Math.floor(Math.random() * wordBank.length);
+    randomWord = new Word(wordBank[randomIndex]);
 }
