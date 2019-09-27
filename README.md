f50bcb6071f544293bc94a751c54b3e67afd5ce6
Caren B <carenpbriones@gmail.com>
2019-09-27 00:35:23 -0700

    Updated README with gifs for each step in gameplay

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Constructor-Word-Guess
-Word Guess game using inquirer and constructors in JavaScript
+This application is a Word Guess game created using inquirer and constructors in JavaScript. Word and Letter objects are defined using constructors, with methods to aid in gameplay.
+
+## Gameplay
+Upon game load, a term is randomly selected from a word bank, which, in this case, is an assortme nt of synonyms for the word "happy." The console will then display a series of blanks that represents each letter of the chosen word, and the user will be prompted (by inquirer) to guess a letter. 
+
+Once the user enters a letter, if the letter is present in the chosen word, each corresponding blank that holds the guessed letter will be filled in with the letter. <br/>
+![correct guess](images/correct-guess.gif)
+
+Otherwise, the letter is not present, the blanks will remain the same, and the user's number of guesses left will be decremented.<br/>
+![incorrect guess](images/incorrect-guess.gif)
+
+The game will continue on guessing different letters until they either finish the word, or run out of guesses. If the user successfully completes the word and the whole word is displayed with no blanks, they have won the game and will be prompted to play again.<br/>
+![user wins game](images/new-game.gif)
+
+If the user runs out of guesses (i.e. "guesses left" is equal to 0), they lose the game, and will be prompted to play again.<br/>
+![user is out of guesses](images/out-of-guesses.gif)
+
+If the user chooses to play again, their number of guesses left will be reset to 10, a new word from the word bank will be chosen, and they will be able to guess the new word.
+
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var Word = require("./word.js");
 var inquirer = require("inquirer");
 
-var wordBank = ["happy", "energetic", "angry", "sleepy", "hungry", "upset"];
+var wordBank = ["blissful", "delighted", "joyous", "jubilant", "ecstatic", "euphoric", "overjoyed", "appeased", "thrilled", "cheerful", "merry", "jubilant"];
 var randomIndex = Math.floor(Math.random() * wordBank.length);
 var randomWord = new Word(wordBank[randomIndex]);
 const MAX_GUESSES = 10;
 var guessesLeft = MAX_GUESSES;
-
 
 playRound();
 
@@ -20,8 +19,10 @@ function playRound() {
         message: "Guess a letter: ",
         type: "input"
     }]).then(function(inquirerResponse) {
+        // Stores current state (or String) of the word prior to the user's guess
         var preGuess = randomWord.convertToString();
 
+        // Guesses if the letter is in the word
         randomWord.guessLetters(inquirerResponse.guess);
         var postGuess = randomWord.convertToString();
 
@@ -30,30 +31,37 @@ function playRound() {
             console.log("\nIncorrect :(");
             guessesLeft -= 1;
             console.log("Guesses left: " + guessesLeft + "\n");
-        } else { // Else, word changed, so guess was correct
+        }
+        // Else, word changed, so guess was correct
+        else {
             console.log("\nCorrect!!!");
         }
 
+        // Game is over
         if (guessesLeft === 0) {
-            // Game is over
             console.log("Game over :(");
             promptNewGame();
-            // Continues game if there are still blanks to fill in
-        } else if (randomWord.convertToString().includes("_")) {
+        }
+        // Continues game if there are still blanks to fill in
+        else if (randomWord.convertToString().includes("_")) {
             playRound();
-        } else { // No more blanks, player has won the game
+        }
+        // No more blanks, player has won the game
+        else {
             console.log("You've won! \n");
             promptNewGame();
         }
     })
 }
 
+// Asks user if they want to play again
 function promptNewGame() {
     inquirer.prompt([{
         name: "playAgain",
         message: "Would you like to play again?",
         type: "confirm"
     }]).then(function(inquirerResponse) {
+        // If user confirms, reset game and play a new round
         if (inquirerResponse.playAgain) {
             resetGame();
             playRound();
@@ -61,6 +69,7 @@ function promptNewGame() {
     })
 }
 
+// Resets game stats to play a new game
 function resetGame() {
     guessesLeft = MAX_GUESSES;
     randomIndex = Math.floor(Math.random() * wordBank.length);
